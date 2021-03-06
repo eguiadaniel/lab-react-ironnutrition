5d7d7b44a22770e1a6d5aa896e85c868c724bba8
eguiadaniel <eguiadaniel@users.noreply.github.com>
2021-03-06 14:45:03 +0100

    Meals listed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Meal from './Meal';
 
 import meals from './meals';
+
+const mealsList = meals.map((meal) => {});
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>Sample App</p>
+        <ul>
+          {meals.map((meal) => {
+            return (
+              <Meal
+                name={meal.name}
+                calories={meal.calories}
+                quantity={meal.quantity}
+                image={meal.image}
+              />
+            );
+          })}
+        </ul>
       </div>
     );
   }
 }
 
 export default App;
+
+// <ul>
+//           {this.state.tasks.map(task => (
+//             <TaskItem
+//               key={task.id}
+//               task={task}
+//               onRemoveTask={() => this.removeTask(task.id)}
+//             />
+//           ))}
+//         </ul>
---
+++ src/Meal.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.scss';
+import meals from './meals';
+
+const Meal = (props) => {
+  return (
+    <div className="Meal">
+      <img src={props.image} alt={props.name} />
+      <div>
+        <h5>{props.name}</h5>
+        <small>{props.calories}</small>
+      </div>
+      <form>
+        <input type="text" />
+        <button>+</button>
+      </form>
+    </div>
+  );
+};
+
+export default Meal;
+
+{
+  /* {
+    "name": "Pizza",
+    "calories": 400,
+    "image": "https://i.imgur.com/eTmWoAN.png",
+    "quantity": 0
+  }, */
+}
